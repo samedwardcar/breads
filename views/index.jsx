e4a762b66f84b80a600281ab761c8669d77bb772
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-09-29 12:16:40 -0500

    adding assignment to github

---
+++ views/index.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads})  {
+    return (
+        <Default>
+        <h2>Index Page</h2>
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        {/* This is a JSX comment. */}
+        <ul>
+          {
+            breads.map((bread, index)=> {
+              return (<li></li>)
+            }) 
+          }
+        </ul>
+      </Default>
+      
+    )
+}
+
+
+module.exports = Index
