9ad6283a159871adf25f1fff49ec70e43413ff4a
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-10-06 07:10:32 -0500

    Redoing to fix broken code Adding 404 page route

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-// DEPENDENCIES
-const express = require('express')
-
-// CONFIGURATION
 require('dotenv').config()
-const PORT = process.env.PORT
+const express = require('express')
 const app = express()
 
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
-
-// ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+    res.send('Hello world!')
 })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
 
-// LISTEN
-app.listen(PORT, () => {
-  console.log('listening on port', PORT);
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
+
+app.listen(process.env.PORT)
